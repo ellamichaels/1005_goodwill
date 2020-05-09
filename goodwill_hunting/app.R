be384fa033517293443ec724e594034637146b2f
Ella Michaels <ellamichaels@Ellas-MacBook-Pro.local>
2020-05-08 20:57:19 -0700

    completed shiny app

--- goodwill_hunting/app.R
+++ goodwill_hunting/app.R
@@ -32,6 +32,8 @@ zip_latlon <- read.csv("US Zip Codes from 2013 Government Data") %>%
 
 zillow <- read.csv("zillow_regions.csv")
 
+goodwill_ratings <- read.csv("goodwill_ratings.csv")
+
 
 
 
@@ -42,38 +44,70 @@ ui <- fluidPage(
     navbarPage("Goodwill Hunting",
                
                # TAB: MAP
-               tabPanel(("Map - Explore"),         
+               tabPanel(("Goodwill Map"),         
                         # Show a plot of the generated distribution
-                        fillPage(textInput("zip_input", "Input Zip Code"),
-                                 selectInput("q_input", "Select Housing Price Quartile", choices = c("All",
+                        fillPage(h2("Goodwill Map"),
+                                 tags$div("It is well known among secondhand shoppers that thrift stores located in wealthier neighborhoods tend to yield better
+                                          finds. Use this tool to find Goodwill locations in neighborhoods with higher home values (a strong proxy for income),
+                                          and explore the relationship between housing price and store reviews."),
+                                 br(),
+                                 tags$div("Input your zip code to view Goodwill locations in your area. Goodwill locations on the map are colored by 
+                                          the quartile of the average home value in their zip code."),
+                                 br(),
+                                 textInput("zip_input", "Input Zip Code"),
+                                 selectInput("q_input", "Select Home Value Quartile", choices = c("All",
                                                                                                      "q1",
                                                                                                      "q2",
                                                                                                      "q3",
                                                                                                      "q4")),
-                                 leafletOutput("goodwill_map", height=500))),                                  
+                                 leafletOutput("goodwill_map", height=500),
+                                 tags$div("*Note that zip codes beginning with zero are not available to search"))),                                  
                
                
                # TAB: STATE BY STATE
                
                tabPanel("Search By State",
                         
-                        mainPanel(textInput("state_input3", "Input State Code"),
+                        mainPanel(h2("Search by State"),
+                                  br(),
+                                  tags$div("Search for a state to view a list of Goodwill locations ranked by the average home value in their 
+                                           zip code. Please use a capitalized, two letter state code (e.g. CA for California)"),
+                                  br(),
+                                  textInput("state_input3", "Input State Code"),
                                   gt_output("by_state"))),
                
                
                # TAB: PLOTS
                tabPanel("Analysis",
+                        h1("Analysis"),
                         tabsetPanel(
                             tabPanel("Yelp Analysis",
-                                     
-                                     mainPanel(textInput("state_input_yelp", "Input State Code", value = "USA"),
-                                               plotlyOutput("yelp_plot"))
+                                     mainPanel(h3("Relationship Between Zip Code Home Value and Yelp Rating"),
+                                     textInput("state_input_yelp", "Input State Code", value = "USA"),
+                                               plotlyOutput("yelp_plot")),
+                                     sidebarPanel(h3("Model"), 
+                                                  gt_output("yelp_gt"),
+                                                  br(),
+                                                  h3("About"),
+                                                  br(),
+                                                  tags$div("Since Yelp does not allow scraping of its site, data on location ratings was collected by manually inputting
+                                                           star ratings of 500 randomly sampled locations. Those that did not have star ratings or were not listed on the Yelp
+                                                           site were omitted. An analysis of this sample did not reveal a statistically significant relationship between average 
+                                                           zip code home value and Yelp rating; the p-value was well in excess of 0.05. The given correlation coefficient was 
+                                                           positive but very small."),
+                                                  br(),
+                                                  tags$div("However, it is worth considering the following before entirely discounting the relationship between neighborhood income and location rating.
+                                                            The largest aggregate variation in home value happens mostly *between* regions, not within them. For instance, 
+                                                           since home values (and living costs overall) are very high in Los Angeles, all of its Goodwill locations are in zip codes
+                                                           in the highest home value quartile. Meanwhile, western Pennsylvania's locations are almost all in the bottom two. Shoppers in
+                                                           each region aren't comparing stores to others nationwide; they're comparing stores to others in their area. Perhaps an analysis
+                                                           grouped by city or region would reveal a stronger relationship."))
                                      
                                      ),
-                            tabPanel("Housing Price Analysis",
+                            tabPanel("Home Value",
                                      
-                                     fillPage( 
-                                         sliderInput("x_good", "Housing Price Range", min = 0, max = 3000000, step = 10000, value = 300000),
+                                     fillPage( h3("Comparing Goodwill Neighborhood Home Values to Nationwide Neighborhood Home Values"),
+                                         sliderInput("x_good", "Home Value Range", min = 0, max = 3000000, step = 10000, value = 300000),
                                          selectInput("region_input", "Select Region", choices = c("United States", 
                                                                                                   "South",
                                                                                                   "Midwest",
@@ -83,33 +117,31 @@ ui <- fluidPage(
                                              splitLayout(cellWidths = c("50%", "50%"), 
                                                          plotlyOutput("goodwills_by_housing_price"),
                                                          plotlyOutput("overall_housing_price"))),
-                                         fluidRow(
-                                             gt_output("zillow_gt"),
-                                             h1(""), # to insert a space between the gt tables
-                                             gt_output("goodwill_gt"))
+                                         br(),
+                                         fluidRow(splitLayout(cellWidths = c("50%", "50%"),
+                                             gt_output("goodwill_gt"), gt_output("zillow_gt")))
                                          
                                      )))),
                
                # TAB: ABOUT
                tabPanel(("About"),
                         h2("About This Project"),
-                        tags$div("It is a well-known fact in the thrifting community that the bougier the neighborhood, the badder the donations, the better the finds. In other words, since most donation-based thrift stores like Goodwill and the Salvation Army source from the immediate surrounding areas, it logically follows that wealthier neighborhoods will tend to have nicer items. This (forthcoming) tool will allow users to optimize their thrift store selections by identifying which Goodwills near them are in neighborhoods with higher average real estate prices, which tends to correspond to household income. It will also (hopefully) do the same for store reviews, and identify any relationship between store rating and neighborhood income. My hypothesis is that stores in wealthier neighborhoods will tend to be more highly rated. "),
-                        h2("How to Navigate the Project"), 
-                        h2("About Me"),
-                        tags$div("Introduction"),
+                        tags$div("It is a well-known fact in the thrifting community that the bougier the neighborhood, the badder the donations, the better the finds. In other words, since most donation-based thrift stores like Goodwill and the Salvation Army source from the immediate surrounding areas, it logically follows that wealthier neighborhoods will tend to have nicer items. This tool allows users to optimize their thrift store selections by identifying which Goodwills near them are in neighborhoods with higher average real estate prices, which tends to correspond to household income. The *Goodwill Map* and *Search by State tabs are two ways to do this. Originally, the intention was to build a search function that pulled all of the locations within a certain radius of an inputted zip code, and sort those locations by average zip code home value. I was able to build a proof-of-concept in an Rmd file, but have yet to figure out how to transfer it to a Shiny App."),
+                        br(),
+                        tags$div("This project also explores the relationship between a location's neighborhood income and its Yelp rating. Since Yelp does not allow scraping of its site, data on location ratings was collected by manually inputting star ratings of 500 randomly sampled locations. Those that did not have star ratings or were not listed on the Yelp site were omitted. An analysis of this sample did not reveal a statistically significant relationship between average zip code home value and Yelp rating, but it is still possible that there is a more significant relationship when examining locations within certain regions, rather than nationwide where for various reasons the distribution may appear more random (more detail in the *Analysis* tab"),
                         h2("Data Sources"),
-                        tags$div("Zillow, Goodwill (scraped locations), Region Data, Zip code coordinates,
-                      Yelp (manual sample scrape, tbd)")),
-               
-               # TAB: MAP - SEARCH
-               
-               tabPanel(("Map - Search"),
-                        
-                        mainPanel(textInput("zip_input2", "Input Zip Code"),
-                                  textInput("state_input2", "Input State Code"),
-                                  leafletOutput("goodwill_map2", height = 500)),
-                        sidebarPanel(dataTableOutput("goodwill_search")))
-               ))
+                        tags$div("Data on average zip code home values was sourced from ",a("Zillow", href = "https://www.zillow.com/research/data/"),". Note that for all zip codes that began with zero, the Zillow dataset dropped the first digit which complicated the process of joining data sets by the zip column."),
+                        br(),
+                        tags$div("Goodwill location data was scraped from ",a("the Goodwill Store Locator", href = "https://www.goodwill.org/locator/"),". Note that 500 locations are omitted from this project. There actually does exist a cleaned, premade ",a("data set", href = "https://www.aggdata.com/aggdata/complete-list-goodwill-industries-locations")," of all Goodwill locations in the US, including their neighborhood, zip code, and store type. It is $99. So that's a no."),
+                        br(),
+                        tags$div("I also used a list of zip code coordinates from ",a("opendatasoft", href = "https://public.opendatasoft.com/explore/dataset/us-zip-code-latitude-and-longitude/export/")," and a list of states by US Census region from a lovely guy called Chris Halpert's ",a("GitHub", href = "https://github.com/cphalpert/census-regions/blob/master/us%20census%20bureau%20regions%20and%20divisions.csv"),"."),
+                        br(),
+                        tags$div("Finally, data on Yelp reviews was manually pulled from ",a("Yelp.com", href = "yelp.com"),"."),
+                        br(),
+                        h2("About Me"),
+                        tags$div("My name is Ella Michaels and I am a sophomore at Harvard University. This is my final project for Gov 1005.
+                                 I study Government and am an avid thrift shopper; I pretty much have not purchased a new item of clothing since 2016 (which admittedly is more a reflection of my shallow bank balance more than my deep commitment to sustainability).
+                                 My GitHub account is linked ",a("here", href = "https://github.com/ellamichaels"),"."))))
 
 
 
@@ -375,7 +407,7 @@ server <- function(input, output) {
                        median = "Median",
                        q3 = "Third Quartile",
                        max = "Maximum") %>%
-            tab_header(title = "Summary Statistics: Nationwide Average Home Prices by Zipcode")
+            tab_header(title = "Summary Statistics: Nationwide")
     )
     
     # Summary statistics for goodwill zipcode housing price average
@@ -396,7 +428,7 @@ server <- function(input, output) {
                        median = "Median",
                        q3 = "Third Quartile",
                        max = "Maximum") %>%
-            tab_header(title = "Summary Statistics: Average Home Prices in Nationwide Goodwill Zipcodes")
+            tab_header(title = "Summary Statistics: Goodwills")
     )
     
     
@@ -412,11 +444,9 @@ server <- function(input, output) {
                 xlim(0,1000000) +
                 geom_point() +
                 geom_smooth(method = "glm", level = 0, aes(group = 1), color = "black") +
-                geom_smooth(level = 0, aes(group = 1), color = "black") +
                 theme_classic() +
                 labs(x = "Average Housing Price in Store Zip Code",
                      y = "Yelp Star Rating",
-                     title = "Relationship Between Neighborhood Housing Price and Yelp Star Rating",
                      color = "Region")
             
         }
@@ -439,6 +469,25 @@ server <- function(input, output) {
         
     })
     
+    # Yelp gt tables
+    
+    output$yelp_gt <- render_gt( {
+        
+        good_ratings_yelp <- goodwill_ratings %>%
+            rename(zip_price = x2020_01)
+        
+        ratings_model <- lm(rating ~ zip_price, data = good_ratings_yelp)
+        
+        ratings_model %>%
+            tidy(conf.int = TRUE) %>%
+            mutate(p.value = round(p.value, 2)) %>%
+            select(term, estimate, std.error, conf.low, conf.high, p.value) %>%
+            gt() %>%
+            fmt_scientific(columns = vars(estimate, std.error, conf.low, conf.high)) %>%
+            cols_label(term = "Variable", estimate = "Estimate", std.error = "Standard Error", 
+                       conf.low = "Lower Bound", conf.high = "Upper Bound", p.value = "p-value") %>%
+            tab_header(title = "Effect of Housing Price on Store Review")
+    })
     
 }
 
